3a118579afc7988473822facb28f77af0f5703a9
prathmeshgajre <prathmgajre2@gmail.com>
2026-02-15 15:45:32 +0530

    code

---
+++ src/main/java/org/githubactions/cicd/GithubActionsApplication.java
@@ -0,0 +1,28 @@
+package org.githubactions.cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GithubActionsApplication {
+
+    @GetMapping("/welcome")
+    public String welcome(){
+        return "welcome..Here !";
+    }
+   /* echo "# Github-Actions" >> README.md
+    git init
+    git add README.md
+    git commit -m "first commit"
+    git branch -M main
+    git remote add origin https://github.com/prathmeshgajre/Github-Actions.git
+    git push -u origin main*/
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubActionsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/githubactions/cicd/GithubActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.githubactions.cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
